091de4e3ae9557a23c39ed3f0dd5a81a655f35ab
Atharv Giri <atharv.giri999@gmail.com>
2026-08-25 22:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (94.66%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+                temp = temp.next;
+            }else{
+                temp.next = list2;
+                list2 = list2.next;
+                temp = temp.next;
+
+            }
+        }
+        
+        // agar fir bhi list 1 me nodes hain to attach krdo list1 ko , nai to list2 ko krdo 
+        temp.next = list1 != null ? list1 : list2;
+
+        return dummy.next;
+        
+    }
+}
